10c506cce201973687f99183a5c7817068eda01c
Alejo9806 <mello0616@gmail.com>
2021-08-29 14:12:37 -0500

    primer commit

---
+++ modules/customer/customer.module.js
@@ -0,0 +1,16 @@
+  
+(function () {
+    'use strict';
+
+    module.exports = init;
+
+    function init() {
+        return {
+            CustomerController: require('./customer.controller'),
+            CustomerMiddleware: require('./customer.middleware'),
+            CustomerService: require('./customer.service'),
+            CustomerModel: require('./customer.model')
+        }
+    }
+
+})();
